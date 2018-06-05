7c5b84074d99895e3805d9c7adb56f413837872c
matthew <matthew.murray@intercom.io>
2018-06-05 16:22:56 +0100

    remove new

---
+++ Pods/Target Support Files/Pods-matttestTests/Pods-matttestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_matttestTests : NSObject
+@end
+@implementation PodsDummy_Pods_matttestTests
+@end
---
+++ Pods/Target Support Files/Pods-matttestUITests/Pods-matttestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_matttestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_matttestUITests
+@end
